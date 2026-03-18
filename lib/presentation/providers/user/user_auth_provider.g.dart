786ef857c47561f795f720f31a2dc832c61d53dc
Ag <choeun8339@naver.com>
2026-03-18 17:03:11 +0900

    feature : auth provider 추가

---
+++ lib/presentation/providers/user/user_auth_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_auth_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userAuthHash() => r'5a316e84470dce58ab2257c6bec5ffd2b342e5ea';
+
+/// 앱 사용자 권한 프로바이더
+///
+/// Copied from [UserAuth].
+@ProviderFor(UserAuth)
+final userAuthProvider = NotifierProvider<UserAuth, User?>.internal(
+  UserAuth.new,
+  name: r'userAuthProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userAuthHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserAuth = Notifier<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
